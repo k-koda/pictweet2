7ca2ce5a073a146b8248cc9c060aad06bbbff5b4
k-koda <keisuke.physi@gmail.com>
2018-06-04 16:45:24 +0900

    詳細画面の実装

--- resources/views/tweets/index.blade.php
+++ resources/views/tweets/index.blade.php
@@ -4,17 +4,16 @@
   <div class="contents row">
     @foreach ($tweets as $tweet)
       <div class="content_post" style="background-image: url({{ $tweet->image }});">
-        @if (Auth::check() && Auth::user()->id == $tweet->user_id )
-          <div class="more">
-            <span><img src="images/arrow_top.png"></span>
-            <ul class="more_list">
-              <li>
-                <a href="/tweets/{{ $tweet->id }}/edit">編集</a>
-                <a href="/tweets/{{ $tweet->id }}/delete">削除</a>
-              </li>
-            </ul>
-          </div>
-        @endif
+        <div class="more">
+          <span><img src="images/arrow_top.png"></span>
+          <ul class="more_list">
+            <li><a href="/tweets/{{ $tweet->id }}">詳細</a></li>
+            @if (Auth::check() && Auth::user()->id == $tweet->user_id)
+              <li><a href="/tweets/{{ $tweet->id }}/edit">編集</a></li>
+              <li><a href="/tweets/{{ $tweet->id }}/delete">削除</a></li>
+            @endif
+          </ul>
+        </div>
         <p>{{ $tweet->text }}</p>
         <span class="name">
           <a href="#">
