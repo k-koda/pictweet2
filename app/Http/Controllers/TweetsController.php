fb0dc44e48079e1a393ea4e38c319a13542c63de
k-koda <keisuke.physi@gmail.com>
2018-06-04 12:07:51 +0900

    最新順に並べる

--- app/Http/Controllers/TweetsController.php
+++ app/Http/Controllers/TweetsController.php
@@ -11,7 +11,7 @@ class TweetsController extends Controller {
 
     public function index() {
 
-      $tweets = Tweet::all();
+      $tweets = Tweet::orderBy('created_at', 'DESC')->get();
       return view('tweets.index') -> with('tweets', $tweets);
 
     }
